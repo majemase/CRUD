6951cb4c9cbe0489d5029a799a5e3b4a156c0d81
majemase <majemase12@gmail.com>
2024-11-20 16:51:25 +0100

    Initial commit

---
+++ app/src/main/java/com/example/crud/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.crud;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        setSupportActionBar(findViewById(R.id.menuprincipal));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.menuadd){
+
+        } else if (item.getItemId() == R.id.menusalir) {
+
+        } else if (item.getItemId() == R.id.menucategoria) {
+
+        } else if (item.getItemId() == R.id.menufecha) {
+
+        }
+        return false;
+    }
+}
+
